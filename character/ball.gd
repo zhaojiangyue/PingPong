b664ea09290ba591b6f671e981cb77e96e4423a2
Jerry Zhao <jiangyue.zh@gmail.com>
2024-02-06 19:19:48 +0800

    ball stop when scored

--- character/ball.gd
+++ character/ball.gd
@@ -2,13 +2,19 @@ extends CharacterBody2D
 
 var direction = Vector2(init_direction(1),init_direction(0.8))
 const SPEED = 300.0
+var Stop
 
 func _physics_process(delta):
 	var collision_info = move_and_collide(direction)
 	if collision_info:
 		direction = direction.bounce(collision_info.get_normal())
-	velocity = direction * SPEED
-	move_and_slide()
+	
+	if Stop:
+		direction = Vector2.ZERO
+	else:
+		velocity = direction * SPEED
+		move_and_slide()
+
 
 func init_direction(float):
 	randomize()
