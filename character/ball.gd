02b4cc204f63cdfb5050b4bd3470c07da418273f
Jerry Zhao <jiangyue.zh@gmail.com>
2024-02-07 00:56:33 +0800

    add sfx

--- character/ball.gd
+++ character/ball.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var sfx_collision = $"../SFX_Collision"
+
 var direction = Vector2(init_direction(1),init_direction(0.8))
 const SPEED = 300.0
 var Stop
@@ -7,6 +9,7 @@ var Stop
 func _physics_process(delta):
 	var collision_info = move_and_collide(direction)
 	if collision_info:
+		sfx_collision.play()
 		direction = direction.bounce(collision_info.get_normal())
 	
 	if Stop:
--- level.gd
+++ level.gd
@@ -2,16 +2,20 @@ extends Node2D
 @onready var score_player = $CanvasLayer/ScorePlayer
 @onready var score_opponent = $CanvasLayer/ScoreOpponent
 @onready var ball = %Ball
+@onready var sfx_score = $SFX_Score
+
 
 
 func _on_opponent_win_body_entered(body):
 	update_score(body,score_opponent)
+	
 
 func _on_player_win_body_entered(body):
 	update_score(body,score_player)
 
 func update_score(body,label):
 	if body == ball:
+		sfx_score.play()
 		label.text = str(int(label.text)+1)
 		body.position = get_viewport_rect().size/2
 		ball.Stop = true
