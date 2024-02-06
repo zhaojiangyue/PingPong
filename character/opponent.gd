6c052c35908742d4bddc746dabf66b384890541c
Jerry Zhao <jiangyue.zh@gmail.com>
2024-02-06 13:49:56 +0800

    opponent move

---
+++ character/opponent.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@onready var ball = get_node("%Ball")
+var up = Vector2(0,-1)
+var down = Vector2(0,1)
+var speed = 300
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if ball.direction.y < 0 && ball.position.y < position.y:
+		velocity = up * speed
+	elif  ball.direction.y > 0 && ball.position.y > position.y:
+		velocity = down * speed
+	move_and_slide()
