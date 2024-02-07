7c30308c907a9efea198cfb29306650072b8f559
Jerry Zhao <jiangyue.zh@gmail.com>
2024-02-07 14:50:47 +0800

    ball countDonw

--- character/ball.gd
+++ character/ball.gd
@@ -4,23 +4,18 @@ extends CharacterBody2D
 
 var direction = Vector2(init_direction(1),init_direction(0.8))
 var speed = 300.0
-var Stop
+var Stop = false
 
 func _physics_process(delta):
-	var collision_info = move_and_collide(direction)
-	if collision_info:
-		sfx_collision.play()
-		direction = direction.bounce(collision_info.get_normal())
-		if speed <1000: #the ball speed up until reach 600
-			speed += 25 
-		print(speed)
-	
-	if Stop:
-		direction = Vector2.ZERO
-	else:
+	if Stop == false:
+		var collision_info = move_and_collide(direction)
+		if collision_info:
+			sfx_collision.play()
+			direction = direction.bounce(collision_info.get_normal())
+			if speed <1000: #the ball speed up until reach 600
+				speed += 25 
 		velocity = direction * speed
 		move_and_slide()
-
 
 func init_direction(float):
 	randomize()
--- level.gd
+++ level.gd
@@ -4,18 +4,28 @@ extends Node2D
 @onready var ball = %Ball
 @onready var sfx_score = $SFX_Score
 
-
+func _process(delta):
+	if ball.Stop == true:
+		$CanvasLayer/CountTime.text = str(int($CanvasLayer/Timer.time_left))
+	else:
+		$CanvasLayer/CountTime.text = ""
 
 func _on_opponent_win_body_entered(body):
 	update_score(body,score_opponent)
-	
+
 
 func _on_player_win_body_entered(body):
 	update_score(body,score_player)
 
+
 func update_score(body,label):
 	if body == ball:
+		$CanvasLayer/Timer.start()
 		sfx_score.play()
 		label.text = str(int(label.text)+1)
 		body.position = get_viewport_rect().size/2
 		ball.Stop = true
+
+
+func _on_timer_timeout():
+	ball.Stop = false # Replace with function body.
