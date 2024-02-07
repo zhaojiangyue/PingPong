1c08fa71a9e3865a2c49218c08ce3fa1e2d0bc1f
Jerry Zhao <jiangyue.zh@gmail.com>
2024-02-07 14:55:14 +0800

    when countdown players reset position

--- character/opponent.gd
+++ character/opponent.gd
@@ -7,9 +7,12 @@ var speed = 300
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	speed = ball.speed #Follow ball speed
-	if ball.direction.y < 0 && ball.position.y < position.y:
-		velocity = up * speed
-	elif  ball.direction.y > 0 && ball.position.y > position.y:
-		velocity = down * speed
-	move_and_slide()
+	if %Ball.Stop == false:
+		speed = ball.speed #Follow ball speed
+		if ball.direction.y < 0 && ball.position.y < position.y:
+			velocity = up * speed
+		elif  ball.direction.y > 0 && ball.position.y > position.y:
+			velocity = down * speed
+		move_and_slide()
+	else:
+		position.y = get_viewport_rect().size.y/2
--- character/player.gd
+++ character/player.gd
@@ -4,10 +4,12 @@ extends CharacterBody2D
 const SPEED = 800.0
 
 func _physics_process(delta):
-	var direction = Input.get_axis("ui_up","ui_down")
-	if direction:
-		velocity.y = direction * SPEED
+	if %Ball.Stop == false:
+		var direction = Input.get_axis("ui_up","ui_down")
+		if direction:
+			velocity.y = direction * SPEED
+		else:
+			velocity.y = move_toward(velocity.y, 0, 100)
+		move_and_slide()
 	else:
-		velocity.y = move_toward(velocity.y, 0, 100)
-
-	move_and_slide()
+		position.y = get_viewport_rect().size.y/2
